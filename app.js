50acc40536ca00a13c4c857608bed39309b59fca
Kanha Ransingh <prakash@909>
2024-01-20 02:28:41 +0530

    changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 5001
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Student = require('./models');
+const cookie = require('cookie-session');
 
 
 mongoose.connect("mongodb+srv://root:akki909@cluster0.sm3rshd.mongodb.net/educations", {})
@@ -14,7 +15,19 @@ mongoose.connect("mongodb+srv://root:akki909@cluster0.sm3rshd.mongodb.net/educat
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error.message);
     });
-
+    
+    app.use(
+        cookie({
+          // Cookie config, take a look at the docs...
+          secret: 'I Love India...',
+          resave: false,
+          saveUninitialized: true,
+          cookie: {
+            secure: true
+          },
+        }),
+      );
+      
 app.use(cors());
 app.use(express.json())
 
